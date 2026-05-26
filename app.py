2a357861f33650e8578c3f1b0a35d7f9e078f283
Harkar-Dinesh <harkardinesh707@gmail.com>
2026-05-26 22:20:27 +0530

    added

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ PORT = 8000
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(2000)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
